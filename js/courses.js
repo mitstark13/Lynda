6a221c2c3f7347d7b79c7226d62083c3d319c993
hopkinsfam <hopkinsfam@gmail.com>
2015-11-10 14:34:13 -0600

    courses left nav dropdown jquery

--- js/courses.js
+++ js/courses.js
@@ -1,4 +1,54 @@
-$('.prjlist a')click(function() {
-	$('courselist.active').removeClass('active');
-	$('projectlist')addClass('active');
-})
+$('.prjList a').click(function(e) {
+	$(this).addClass('active');
+	$('.crsList a').removeClass('active')
+	$('.courseList').removeClass('activeTab');
+	$('.projectList').addClass('activeTab');
+	e.preventDefault();
+});   
+
+$('.crsList a').click(function(e) {
+	$(this).addClass('active');
+	$('.prjList a').removeClass('active')
+	$('.projList').removeClass('activeTab');
+	$('.courseList').addClass('activeTab');
+	e.preventDefault();
+}); 
+
+/* Video Details tabs - the e.preventDefault makes
+it so link doesn't jump it to top of page */
+$('.detailHeader li a').click(function(e) {
+$('.detailHeader li a.active').removeClass('active');
+    $(this).toggleClass('active');
+ 	 e.preventDefault();
+ });
+
+
+	
+
+
+
+
+$('.courseList ul li img').not( ".submenu img" ).click(function() {
+  var clicks = $(this).data('clicks');
+  if (clicks) {
+     $(this).transition({ rotate: '0deg' },500,'ease');
+     $(this).siblings('.submenu').hide('slow');
+  } else {	
+     $(this).transition({ rotate: '90deg' },500,'ease');
+     $(this).siblings('.submenu').show('slow');
+  }
+  $(this).data("clicks", !clicks);
+});
+
+$('.expand').click(function(e) {
+     $('.courseList ul li img').not( ".submenu img" ).transition({ rotate: '90deg' },500,'ease');
+ 	 $( ".submenu" ).show( "slow" );
+ 	 e.preventDefault();
+});
+
+$('.collapse').click(function(e) {	
+   	$('.courseList ul li img').not( ".submenu img" ).transition({ rotate: '0deg' },500,'ease');  
+ 	$( ".submenu" ).hide( "slow" );
+ e.preventDefault();
+
+});
