c8f7b521f99a1f6c3f7a690d6204f88e67e62136
mitstark13 <starkeymitchell3@gmail.com>
2015-11-09 19:15:27 -0600

    communities header looking sexy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $('.navbar .search img').click(function() {
-	$('.search input').css('display', 'block')
+	$('.search input').animate({opacity: '1'});
+	$('.search input').animate({width: '250px'}, "slow");
+	$('.search input').css('display', 'block');
+});
+$('.search input').keypress(function() {
+	$('.searchDrop').css('display', 'block');
+});
+$('.searchDrop').mouseleave(function() {
+	$('.searchDrop').css('display', 'none');
+});
+$('.courses').mouseenter(function() {
+	$('.fullDrop').css('display', 'block');
+});
+$('.dropdown').mouseleave(function() {
+	$('.fullDrop').css('display', 'none');
 });
