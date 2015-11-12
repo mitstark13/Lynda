2dd4406701fa956589dd52a40e6e1b5d3707080f
mitstark13 <starkeymitchell3@gmail.com>
2015-11-11 20:21:41 -0600

    pause button and more java

--- js/comment.js
+++ js/comment.js
@@ -7,5 +7,8 @@ $('.writeReply button:last-of-type').click(function(){
 $('.writeReply button:first-of-type').click(function(){
 	$('.reply').css('display', 'none');
 	var reply = ($('.writeReply textarea').val());
-	$('.comments ul li:first-child').after('<img src=img/replyArrow.png class=replyArrow> <li class=newComment> <div class=comment> <h3>John Wright | Some job at Salesforce</h3> <img src=img/john.jpg> <p>' + reply + '</p> <small>Posted 0 minutes ago</small> <button>Reply</button></li>');
+	$('.comments ul li:first-child').after('<div class="fullReply"><img src=img/replyArrow.png class=replyArrow> <li class=newComment> <div class=comment> <h3>John Wright | Some job at Salesforce</h3> <img src=img/john.jpg> <p>' + reply + '</p> <small>Posted 0 minutes ago</small> <button>Reply</button></li></div>');
+	$('main').height(function (index, height) {
+    return (height + 210);
+	});
 });
---
+++ js/communities.js
@@ -0,0 +1,12 @@
+$('.searchCom input').keypress(function() {
+	$('.comDrop').css('display', 'block');
+});
+$('.searchCom input').mouseleave(function() {
+	$('.comDrop').css('display', 'none');
+})
+$('.comDrop').mouseleave(function() {
+	$('.comDrop').css('display', 'none');
+})
+$('.comDrop').mouseenter(function() {
+	$('.comDrop').css('display', 'block');
+})
